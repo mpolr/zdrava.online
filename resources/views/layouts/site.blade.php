65abe0e0f05c06b3cef7d35f68b308c91d291d33
Pavel Monahov <monahov@giftery.ru>
2023-05-29 03:53:08 +0300

    Local Google font 'Ubuntu'

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -9,7 +9,7 @@
     <title>{{ config('app.name', 'Zdrava') }}</title>
 
     <!-- Fonts -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    @googlefonts('ubuntu')
 
     <!-- Styles -->
     @vite('resources/css/app.css')
