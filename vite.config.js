84c231dca9557b937b64cb097dced9cee8c91ca9
Pavel Monahov <monahov@giftery.ru>
2023-05-30 11:36:29 +0300

    Add laravel livewire

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import manifestSRI from 'vite-plugin-manifest-sri';
+import livewire from "@defstudio/vite-livewire-plugin";
 
 export default defineConfig({
     plugins: [
