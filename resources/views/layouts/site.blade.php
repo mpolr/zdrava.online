181715fe84ad14e160d45b2c1c782032f8a0f727
Pavel Monahov <monahov@giftery.ru>
2023-06-03 16:50:57 +0300

    Add Matomo analytics script

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -19,6 +19,21 @@
     @vite('resources/js/app.js')
     @livewireScripts
     @yield('js')
+    <!-- Matomo -->
+    <script>
+        var _paq = window._paq = window._paq || [];
+        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+            var u="//stat.mpolr.ru/";
+            _paq.push(['setTrackerUrl', u+'matomo.php']);
+            _paq.push(['setSiteId', '1']);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+        })();
+    </script>
+    <!-- End Matomo Code -->
 </head>
 <body>
     <section class="mb-40">
