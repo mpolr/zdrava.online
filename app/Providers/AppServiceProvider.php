f3b03b6ce7bcc1d22415a67b340360e6ce7e468a
Pavel Monahov <monahov@giftery.ru>
2023-06-06 04:41:23 +0300

    Disable debug, enable debugbar only for user with ID = 1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Event;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,6 +21,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Event::listen(
+            \Illuminate\Auth\Events\Authenticated::class,
+            function () {
+                if (\Auth::id() != 1) {
+                    \Debugbar::disable();
+                }
+            }
+        );
         URL::forceScheme('https');
     }
 }
