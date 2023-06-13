f03ba2f21ae70254d9d2b4b049971b5a310eeb08
Pavel Monahov <monahov@giftery.ru>
2023-06-14 02:43:21 +0300

    Activities in athelte profile page

--- resources/views/livewire/athlete/profile.blade.php
+++ resources/views/livewire/athlete/profile.blade.php
@@ -21,24 +21,75 @@
                 </h2>
             </div>
             <hr class="h-px mt-4 mb-8 bg-gray-200 border-0 dark:bg-gray-700">
-            <div class="flex items-center w-full">
-                <div class="relative inline-flex items-center justify-center w-24 h-24 mb-3 overflow-hidden bg-gray-300 rounded-full dark:bg-gray-600">
-                    <span class="font-bold text-3xl text-gray-600 dark:text-gray-300">
-                        {{ $user->getInitials() }}
-                    </span>
-                </div>
-                <div class="relative inline-flex items-center justify-center w-24 h-24 mb-3 overflow-hidden bg-gray-300 rounded-full dark:bg-gray-600">
-                    <span class="font-bold text-3xl text-gray-600 dark:text-gray-300">
-                        {{ $user->getInitials() }}
-                    </span>
-                </div>
-            </div>
         </div>
         <div class="w-4/4 ml-4">
-            <div>
+            <div class="pl-4">
                 @error('profile') @livewire('toast.errors') @enderror
                 @if (Session::get('success'))
                     @livewire('toast.success')
+                @endif
+                @if($user->activities)
+                    @foreach($user->activities as $activity)
+                        <div class="max-w-full mb-3 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                            <div class="p-4">
+                                <div class="flex gap-x-4">
+                                    @if($activity->getUser()->getPhoto())
+                                        <img class="h-12 w-12 flex-none rounded-full bg-gray-50" src="{{ $activity->getUser()->getPhoto() }}" alt="{{ $activity->getUser()->getFullName() }}">
+                                    @else
+                                        <a href="{{ route('settings.profile') }}">
+                                            <div class="relative inline-flex items-center justify-center w-12 h-12 mb-3 overflow-hidden bg-gray-300 rounded-full dark:bg-gray-600">
+                                            <span class="font-bold text-xl text-gray-600 dark:text-gray-300">
+                                                {{ $activity->getUser()->getInitials() }}
+                                            </span>
+                                            </div>
+                                        </a>
+                                    @endif
+                                    <div class="min-w-0 flex-auto">
+                                        <p class="text-sm font-semibold leading-6 text-gray-900">{{ $activity->getUser()->getFullName() }}</p>
+                                        <p class="mt-1 truncate text-xs leading-5 text-gray-500">
+                                            {{ $activity->getLongStartDate() }} - {{ $activity->getCountry() }}@if($activity->locality), {{ $activity->locality }}@endif
+                                        </p>
+                                        <a href="{{ route('activities.get', $activity->id) }}">
+                                            <h5 class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                                {{ $activity->name }}
+                                            </h5>
+                                        </a>
+                                        <div class="grid text-sm mt-4 grid-cols-1 gap-6 sm:grid-cols-3">
+                                            <a href="{{ route('athlete.subscriptions') }}">
+                                                <p class="mb-3 text-left text-gray-600 dark:text-gray-400">
+                                                    {{ __('Distance') }}<br />
+                                                    <span class="text-black text-lg font-semibold">{{ __(':distance km', ['distance' => $activity->getDistance()]) }}</span>
+                                                </p>
+                                            </a>
+                                            <a href="{{ route('athlete.subscribers') }}">
+                                                <p class="mb-3 text-left text-gray-600 dark:text-gray-400">
+                                                    {{ __('Elevation') }}<br />
+                                                    <span class="text-black text-lg font-semibold">{{ __(':elevation m', ['elevation' => $activity->elevation_gain]) }}</span>
+                                                </p>
+                                            </a>
+                                            <a href="{{ route('athlete.training') }}">
+                                                <p class="mb-3 text-left text-gray-600 dark:text-gray-400">
+                                                    {{ __('Duration') }}<br />
+                                                    <span class="text-black text-lg font-semibold">{{ $activity->getDuration() }}</span>
+                                                </p>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div>
+
+                                </div>
+                            </div>
+                            <a href="#">
+                                <img class="max-h-60" src="{{ $activity->getImage() }}" alt="" />
+                            </a>
+                            <div class="p-0">
+
+                            </div>
+                        </div>
+                    @endforeach
+                @else
+                    // TODO: Нет тренировок
                 @endif
             </div>
         </div>
