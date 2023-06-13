d27b09710d709bf606609a5b31fa4ca8d3d7c16a
Pavel Monahov <monahov@giftery.ru>
2023-06-14 02:31:48 +0300

    Fix activity user initials in dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -90,7 +90,7 @@
                                         <a href="{{ route('settings.profile') }}">
                                             <div class="relative inline-flex items-center justify-center w-12 h-12 mb-3 overflow-hidden bg-gray-300 rounded-full dark:bg-gray-600">
                                             <span class="font-bold text-xl text-gray-600 dark:text-gray-300">
-                                                {{ Auth::user()->getInitials() }}
+                                                {{ $activity->getUser()->getInitials() }}
                                             </span>
                                             </div>
                                         </a>
