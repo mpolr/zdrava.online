d9d469c3fcda9c256aa899be017f66fca75e56dc
Pavel Monahov <monahov@giftery.ru>
2023-06-17 04:41:12 +0300

    Dropdown menu for activity view

--- resources/views/activities/view.blade.php
+++ resources/views/activities/view.blade.php
@@ -17,6 +17,29 @@
                         {{ __('Analysis') }}
                     </a>
                 </div>
+                <div class="flex pt-4">
+                    <button id="dropdownButton" data-dropdown-toggle="dropdown" class="inline-block text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-1.5" type="button">
+                        <span class="sr-only">Open dropdown</span>
+                        <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z"></path></svg>
+                    </button>
+                    <!-- Dropdown menu -->
+                    <div id="dropdown" class="z-10 hidden text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                        <ul class="py-2" aria-labelledby="dropdownButton">
+                            <li>
+                                <a href="{{ $activity->getGPXFile() }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+                                    {{ __('Download :file', ['file' => 'GPX']) }}
+                                </a>
+                            </li>
+                            @if ($activity->getUser()->id == auth()->user()->id)
+                            <li>
+                                <a href="#" class="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+                                    {{ __('Delete') }}
+                                </a>
+                            </li>
+                            @endif
+                        </ul>
+                    </div>
+                </div>
             </div>
             <div class="w-3/4">
                 <div id="accordion-flush" data-accordion="collapse" data-active-classes="bg-white dark:bg-gray-900 text-gray-900 dark:text-white" data-inactive-classes="text-gray-500 dark:text-gray-400">
