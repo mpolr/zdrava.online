48c7b585b6ef7471e57f7f1e3225651bdea199c9
Pavel Monahov <mpolr21@gmail.com>
2023-10-02 09:17:12 +0300

    Site: Fix settings pages layout

--- resources/views/livewire/settings/account.blade.php
+++ resources/views/livewire/settings/account.blade.php
@@ -1,24 +1,31 @@
 @section('title', __('My account') . ' | Zdrava')
-<div>
-    <h2 class="mb-2 mt-0 text-4xl font-medium leading-tight text-black">
-        {{ __('My account') }}
-    </h2>
-    <hr class="h-px mt-4 mb-8 bg-gray-200 border-0 dark:bg-gray-700">
-    @error('account') @livewire('toast.errors') @enderror
-    @if (Session::get('success'))
-        @livewire('toast.success')
-    @endif
-    <label for="locale">{{ __('Interface language') }}</label>
-    <select wire:model="locale" name="locale" id="locale" class="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer">
-        @foreach(config('app.available_locales') as $locale_name => $available_locale)
-            @if($available_locale === $locale)
-                <option value="{{ $available_locale }}" selected>{{ $locale_name }}</option>
-            @else
-                <option value="{{ $available_locale }}">{{ $locale_name }}</option>
+<main class="container mx-auto px-0 py-12 max-w-screen-lg">
+    <div class="grid grid-cols-3 gap-4">
+        <div class="w-full">
+            @include('components.settings-menu')
+        </div>
+        <div class="w-full col-span-2">
+            <h2 class="mb-2 mt-0 text-4xl font-medium leading-tight text-black">
+                {{ __('My account') }}
+            </h2>
+            <hr class="h-px mt-4 mb-8 bg-gray-200 border-0 dark:bg-gray-700">
+            @error('profile') @livewire('toast.errors') @enderror
+            @if (session()->get('success'))
+                @livewire('toast.success')
             @endif
-        @endforeach
-    </select>
-    <p class="pt-4">
-        <button wire:click="save" type="button" class="px-4 py-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">{{ __('Save') }}</button>
-    </p>
-</div>
+            <label for="locale">{{ __('Interface language') }}</label>
+            <select wire:model="locale" name="locale" id="locale" class="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer">
+                @foreach(config('app.available_locales') as $locale_name => $available_locale)
+                    @if($available_locale === $locale)
+                        <option value="{{ $available_locale }}" selected>{{ $locale_name }}</option>
+                    @else
+                        <option value="{{ $available_locale }}">{{ $locale_name }}</option>
+                    @endif
+                @endforeach
+            </select>
+            <p class="pt-4">
+                <button wire:click="save" type="button" class="px-4 py-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">{{ __('Save') }}</button>
+            </p>
+        </div>
+    </div>
+</main>
--- resources/views/livewire/settings/privacy.blade.php
+++ resources/views/livewire/settings/privacy.blade.php
@@ -1,18 +1,25 @@
 @section('title', __('Privacy') . ' | Zdrava')
-<div>
-    <h2 class="mb-2 mt-0 text-4xl font-medium leading-tight text-black">
-        {{ __('Privacy') }}
-    </h2>
-    <hr class="h-px mt-4 mb-8 bg-gray-200 border-0 dark:bg-gray-700">
-    @error('privacy') @livewire('toast.errors') @enderror
-    @if (Session::get('success'))
-        @livewire('toast.success')
-    @endif
-    <div>
-        <label class="relative inline-flex items-center cursor-pointer">
-            <input wire:model="private" type="checkbox" value="" class="sr-only peer">
-            <div class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-            <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">{{ __('Private profile') }}</span>
-        </label>
+<main class="container mx-auto px-0 py-12 max-w-screen-lg">
+    <div class="grid grid-cols-3 gap-4">
+        <div class="w-full">
+            @include('components.settings-menu')
+        </div>
+        <div class="w-full col-span-2">
+            <h2 class="mb-2 mt-0 text-4xl font-medium leading-tight text-black">
+                {{ __('Privacy') }}
+            </h2>
+            <hr class="h-px mt-4 mb-8 bg-gray-200 border-0 dark:bg-gray-700">
+            @error('profile') @livewire('toast.errors') @enderror
+            @if (session()->get('success'))
+                @livewire('toast.success')
+            @endif
+            <div>
+                <label class="relative inline-flex items-center cursor-pointer">
+                    <input wire:model="private" type="checkbox" value="" class="sr-only peer">
+                    <div class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                    <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">{{ __('Private profile') }}</span>
+                </label>
+            </div>
+        </div>
     </div>
-</div>
+</main>
