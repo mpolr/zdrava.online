75dd6a78b96b5cf5cfc4da8e4c76bc890dbcea25
Pavel Monahov <mpolr21@gmail.com>
2023-10-23 10:50:16 +0300

    Site: Add footer margin

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -223,7 +223,7 @@
             @endif
         </main>
     </div>
-    <footer class="bg-white dark:bg-gray-900">
+    <footer class="bg-white dark:bg-gray-900 mt-10">
         <div class="mx-auto w-full max-w-screen-xl">
             <div class="grid grid-cols-2 gap-8 px-4 py-6 lg:py-8 md:grid-cols-4">
                 <div>
