ce00cf931de7d9d2af6cb33002ddf0f99e9f1540
Pavel Monahov <mpolr21@gmail.com>
2023-10-26 23:38:39 +0300

    Site: Test login page

--- tests/Feature/PrimaryTest.php
+++ tests/Feature/PrimaryTest.php
@@ -7,15 +7,21 @@ use Tests\TestCase;
 
 class PrimaryTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
     public function testIndexPageIsOk(): void
     {
-        $response = $this->get('/');
+        $response = $this->get(route('index'));
 
         $response->assertStatus(200);
         $response->assertViewIs('index');
         $response->assertSeeText('Zdrava');
+    }
+
+    public function testLoginPageIsOk(): void
+    {
+        $response = $this->get(route('auth.login'));
+
+        $response->assertStatus(200);
+        $response->assertViewIs('auth.login');
+        $response->assertSeeText(__('Remember me'));
     }
 }
