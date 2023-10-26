d7cc63b9f6f7eb72e320f59f68540b979bef07a7
Pavel Monahov <mpolr21@gmail.com>
2023-10-26 19:27:57 +0300

    Site: Fix buttons sizes

--- resources/views/livewire/search/users.blade.php
+++ resources/views/livewire/search/users.blade.php
@@ -61,21 +61,21 @@
                                         @if(!in_array($user->id, $subscriptions))
                                             @if (!in_array($user->id, $awaiting))
                                                 @if (!$user->private)
-                                                    <button wire:click="subscribe({{ $user }})" type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+                                                    <button wire:click="subscribe({{ $user }})" type="button" class="w-32 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
                                                         {{ __('Subscribe') }}
                                                     </button>
                                                 @else
-                                                    <button wire:click="subscribe({{ $user }})" type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">
+                                                    <button wire:click="subscribe({{ $user }})" type="button" class="w-32 focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">
                                                         {{ __('Subscription request') }}
                                                     </button>
                                                 @endif
                                             @else
-                                                <button wire:click="cancel({{ $user }})" type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">
+                                                <button wire:click="cancel({{ $user }})" type="button" class="w-32 focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">
                                                     {{ __('Cancel request') }}
                                                 </button>
                                             @endif
                                         @else
-                                            <button wire:click="unsubscribe({{ $user }})" type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+                                            <button wire:click="unsubscribe({{ $user }})" type="button" class="w-32 focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
                                                 {{ __('Unsubscribe') }}
                                             </button>
                                         @endif
