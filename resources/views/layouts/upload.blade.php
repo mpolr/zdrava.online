4c25b81d33430861ed3e6ce318af1f47c6891345
Pavel Monahov <mpolr21@gmail.com>
2023-11-13 04:22:50 +0300

    Site: Add link 'Import from Strava'

--- resources/views/layouts/upload.blade.php
+++ resources/views/layouts/upload.blade.php
@@ -11,6 +11,9 @@
                     <a href="{{ route('upload.workout') }}" type="button" class="-mt-px py-3 px-4 inline-flex justify-left items-center gap-2 border font-medium bg-white text-gray-700 align-middle hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-2 focus:ring-blue-600 transition-all text-sm dark:bg-gray-800 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400">
                         {{ __('Manually add') }}
                     </a>
+                    <a href="#" type="button" class="-mt-px py-3 px-4 inline-flex justify-left items-center gap-2 border font-medium bg-white text-gray-700 align-middle hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-2 focus:ring-blue-600 transition-all text-sm dark:bg-gray-800 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400">
+                        {{ __('Import from Strava') }}
+                    </a>
                 </div>
             </div>
             @yield('upload.content')
