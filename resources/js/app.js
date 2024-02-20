90571e112b4553eabc9f5768ff2fe5ba34769c8d
Pavel Monahov <mpolr21@gmail.com>
2024-02-20 15:13:13 +0300

    Добавил пакет 'masmerise/livewire-toaster'

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,9 @@ import './bootstrap';
 import { initTE } from "tw-elements";
 initTE();
 import 'flowbite';
+
+import Alpine from 'alpinejs';
+import Toaster from '../../vendor/masmerise/livewire-toaster/resources/js'; // 👈
+Alpine.plugin(Toaster);
+window.Alpine = Alpine;
+Alpine.start();
