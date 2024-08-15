d329d2376a789b51d27ae2ef6532bad24b82c85b
Pavel Monahov <mpolr21@gmail.com>
2024-08-15 14:03:56 +0300

    Site: Log admin panel access attempt

--- app/Http/Livewire/Admin/Index.php
+++ app/Http/Livewire/Admin/Index.php
@@ -3,13 +3,16 @@
 namespace App\Http\Livewire\Admin;
 
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Log;
 use Livewire\Component;
+use Psr\Log\LogLevel;
 
 class Index extends Component
 {
     public function render(): View|\Illuminate\View\View
     {
         if (!auth()->user()->hasRole('admin')) {
+            Log::channel('telegram')->log(LogLevel::NOTICE, auth()->user()->getFullName() . " try access admin panel");
             abort(403, 'Insufficient access rights');
         }
 
