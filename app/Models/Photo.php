af48283dff2ef2bd77eb23fc9e0160496b000747
Pavel Monahov <mpolr21@gmail.com>
2024-10-18 16:36:40 +0300

    API: Support for photos in activity

---
+++ app/Models/Photo.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Photo extends Model
+{
+    protected $table = 'photos';
+
+    protected $fillable = [
+        'activities_id',
+        'users_id',
+        'url',
+        'description',
+    ];
+}
