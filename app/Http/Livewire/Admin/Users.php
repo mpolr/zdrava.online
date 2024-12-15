503eaf3ad661d371214cffd0b017add949538b71
Pavel Monahov <mpolr21@gmail.com>
2024-12-15 22:38:35 +0300

    SIte: User search and pagination in admin panel

--- app/Http/Livewire/Admin/Users.php
+++ app/Http/Livewire/Admin/Users.php
@@ -4,22 +4,40 @@ namespace App\Http\Livewire\Admin;
 
 use App\Models\User;
 use Illuminate\Contracts\View\View;
-use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Livewire\Component;
 
 class Users extends Component
 {
-    private Collection $users;
+    private LengthAwarePaginator $users;
+    public string $search = '';
+    protected array $rules = [
+        'search' => 'string|min:1|max:64',
+    ];
 
-    public function mount(): void
+    public function __construct()
     {
-        $this->users = User::all();
+        parent::__construct();
+        $this->users = User::paginate(50);
+    }
+
+    public function search(): void
+    {
+        $this->validate();
+
+        if (!empty($this->search)) {
+            $this->users = User::where('first_name', 'LIKE', "%{$this->search}%")
+                ->orWhere('last_name', 'LIKE', "%{$this->search}%")
+                ->orWhere('nickname', 'LIKE', "%{$this->search}%")
+                ->orWhere('email', 'LIKE', "%{$this->search}%")
+                ->paginate(50);
+        }
     }
 
     public function render(): View|\Illuminate\View\View
     {
         return view('livewire.admin.users', [
-            'users' => $this->users
+            'users' => $this->users,
         ]);
     }
 }
--- resources/views/livewire/admin/users.blade.php
+++ resources/views/livewire/admin/users.blade.php
@@ -16,9 +16,20 @@
                         <path d="M100 15a85 85 0 1 0 85 85 84.93 84.93 0 0 0-85-85Zm0 150a65 65 0 1 1 65-65 64.87 64.87 0 0 1-65 65Zm16.5-107.5a9.67 9.67 0 0 0-14 0L74 86a19.92 19.92 0 0 0 0 28.5l28.5 28.5a9.9 9.9 0 0 0 14-14l-28-29L117 71.5c3.5-3.5 3.5-10-.5-14Z"/>
                     </svg>
                 </a>
-                {{ __('Users: :count', ['count' => count($users)]) }}
+                {{ __('Users') }}: {{ __(':count from :total', ['count' => $users->firstItem() . '-' . $users->lastItem(), 'total' => $users->total()]) }}
             </h4>
-            <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <label for="search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">{{ __('Search') }}</label>
+            <div class="relative">
+                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                    <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                </div>
+                <input wire:model="search" type="search" id="search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                       placeholder="{{ __('Name, nickname or email...') }}" required>
+                <button wire:click="search" type="button" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    {{ __('Search') }}
+                </button>
+            </div>
+            <div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-4">
                 <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                     <thead class="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
@@ -141,6 +152,9 @@
                     @endif
                     </tbody>
                 </table>
+            </div>
+            <div class="mt-6">
+                {{ $users->links() }}
             </div>
         </div>
     </div>
