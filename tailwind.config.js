83eeda0edab68d75d54078e12238961ca932aa67
Pavel Monahov <mpolr21@gmail.com>
2024-12-15 07:28:36 +0300

    Updated npm dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,7 @@ module.exports = {
     content: [
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
-        "./resources/**/*.vue",
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        "./node_modules/tw-elements/dist/js/**/*.js",
         "./node_modules/flowbite/**/*.js",
     ],
     theme: {
@@ -13,7 +11,6 @@ module.exports = {
     },
     darkMode: "class",
     plugins: [
-        require("tw-elements/dist/plugin.cjs"),
         require('flowbite/plugin'),
     ]
-}
+};
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import manifestSRI from 'vite-plugin-manifest-sri';
-import 'flowbite';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
         manifestSRI(),
