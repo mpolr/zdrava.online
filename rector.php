0614441012895b5637b7fa86f90bc13f0c961e8e
Pavel Monahov <mpolr21@gmail.com>
2025-01-20 17:22:35 +0300

    Updare rector and fix errors

--- rector.php
+++ rector.php
@@ -8,18 +8,15 @@ use Rector\DeadCode\Rector\ClassMethod\RemoveUselessParamTagRector;
 use Rector\DeadCode\Rector\ClassMethod\RemoveUselessReturnTagRector;
 use Rector\DeadCode\Rector\Property\RemoveUselessVarTagRector;
 use Rector\TypeDeclaration\Rector\Class_\ReturnTypeFromStrictTernaryRector;
-use Rector\TypeDeclaration\Rector\ClassMethod\BoolReturnTypeFromStrictScalarReturnsRector;
 use Rector\TypeDeclaration\Rector\ClassMethod\NumericReturnTypeFromStrictScalarReturnsRector;
 use Rector\TypeDeclaration\Rector\ClassMethod\ReturnNeverTypeRector;
 use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromReturnDirectArrayRector;
 use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromReturnNewRector;
-use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictBoolReturnExprRector;
 use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictConstantReturnRector;
 use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictFluentReturnRector;
 use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictNativeCallRector;
 use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictNewArrayRector;
 use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictParamRector;
-use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictScalarReturnExprRector;
 use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictTypedCallRector;
 use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictTypedPropertyRector;
 use RectorLaravel\Set\LaravelSetList;
@@ -39,7 +36,6 @@ return static function (RectorConfig $rectorConfig): void {
         LaravelSetList::LARAVEL_100
     ]);
 
-    $rectorConfig->rule(BoolReturnTypeFromStrictScalarReturnsRector::class);
     $rectorConfig->rule(NumericReturnTypeFromStrictScalarReturnsRector::class);
     $rectorConfig->rule(RemoveUselessVarTagRector::class);
     $rectorConfig->rule(RemoveUselessParamTagRector::class);
@@ -47,13 +43,11 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rule(ReturnNeverTypeRector::class);
     $rectorConfig->rule(ReturnTypeFromReturnDirectArrayRector::class);
     $rectorConfig->rule(ReturnTypeFromReturnNewRector::class);
-    $rectorConfig->rule(ReturnTypeFromStrictBoolReturnExprRector::class);
     $rectorConfig->rule(ReturnTypeFromStrictConstantReturnRector::class);
     $rectorConfig->rule(ReturnTypeFromStrictFluentReturnRector::class);
     $rectorConfig->rule(ReturnTypeFromStrictNativeCallRector::class);
     $rectorConfig->rule(ReturnTypeFromStrictNewArrayRector::class);
     $rectorConfig->rule(ReturnTypeFromStrictParamRector::class);
-    $rectorConfig->rule(ReturnTypeFromStrictScalarReturnExprRector::class);
     $rectorConfig->rule(ReturnTypeFromStrictTernaryRector::class);
     $rectorConfig->rule(ReturnTypeFromStrictTypedCallRector::class);
     $rectorConfig->rule(ReturnTypeFromStrictTypedPropertyRector::class);
