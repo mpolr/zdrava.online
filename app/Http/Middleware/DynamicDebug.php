02bee3a1d9d45d4642477f61af1dfa3867fa96e2
Pavel Monahov <mpolr21@gmail.com>
2025-02-19 21:05:46 +0300

    Remove debugbar

--- app/Http/Middleware/DynamicDebug.php
+++ app/Http/Middleware/DynamicDebug.php
@@ -10,16 +10,12 @@ class DynamicDebug
 {
     public function handle(Request $request, Closure $next)
     {
-        debugbar()->disable();
-
         $response = $next($request);
 
         if (Auth::check() && auth()->user()->hasRole('admin')) {
             config([
                 'app.debug' => 'true',
             ]);
-            debugbar()->enable();
-            debugbar()->modifyResponse($request, $response);
         }
 
         return $response;
